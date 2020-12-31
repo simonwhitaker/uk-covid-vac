c3d7faf75797d686beac64bb2418fc63f6e428a1
Simon Whitaker <sw@netcetera.org>
2020-12-31 13:21:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import csv
+
+from uk_covid19 import Cov19API
+
+# For valid filter and structure values, see
+# https://coronavirus.data.gov.uk/details/developers-guide
+
+filters = [
+    'areaType=overview'
+]
+
+structure = {
+    'date': 'date',
+    'newPeopleReceivingFirstDose': 'newPeopleReceivingFirstDose',
+    'cumPeopleReceivingFirstDose': 'cumPeopleReceivingFirstDose',
+    'newPeopleReceivingSecondDose': 'newPeopleReceivingSecondDose',
+    'cumPeopleReceivingSecondDose': 'cumPeopleReceivingSecondDose',
+}
+
+outfile = 'data.csv'
+
+api = Cov19API(filters, structure)
+data = api.get_json()
+
+with open(outfile, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=structure.keys())
+    writer.writeheader()
+    for row in data['data']:
+        writer.writerow(row)
